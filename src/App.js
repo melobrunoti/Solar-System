168cfe50418b54521a24fd1ead077ce4339300ce
Bruno Melo <melobrunoti@gmail.com>
2021-10-29 17:44:10 -0300

    Requisitos 2-3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-         <Header/>
+        <Header />
+        <SolarSystem />
+        <Title />
       </div>
     );
   }
