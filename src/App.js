a781eeb344ea54fe08e8cb2d251b6d980a6bd52c
Bruno Melo <melobrunoti@gmail.com>
2021-10-29 18:55:29 -0300

    Requisito 6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
-import Planets from './data/planets';
 
 class App extends React.Component {
   render() {
@@ -11,8 +8,7 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
-        <Title headline="Planetas" />
-        <PlanetCard planetName="Terra" />
+
       </div>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system" />
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        {Planets.map(({ name, image }) => (
+          <PlanetCard key={ name } planetName={ name } planetImage={ image } />
+        ))}
+      </div>
     );
   }
 }
